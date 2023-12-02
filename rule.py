ffd9e267150ef3b0a96f6d2f2803b7d462ecdc1c
mboyd020 <mboyd020@ucr.edu>
2023-12-02 00:31:55 -0800

    Added comments/gitignore

--- rule.py
+++ rule.py
@@ -1,6 +1,6 @@
 class Rule:
     rule = ""
-    ruleStack = []
+    ruleStack = [] #technically doesn't enforce use as a stack but if it's not used as a stack it could be a problem
     def parseRule ():
         pass
     def checkRule():
