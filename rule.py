924567cde2c748f54493d8a6d505e7cf752caf50
mboyd020 <mboyd020@ucr.edu>
2023-12-02 00:37:25 -0800

    added branch

--- rule.py
+++ rule.py
@@ -1,8 +1,10 @@
 class Rule:
-    rule = ""
     ruleStack = [] #technically doesn't enforce use as a stack but if it's not used as a stack it could be a problem
-    def parseRule ():
-        pass
+    def __init__(self, rule): 
+        self.rule = rule
+    def parseRule (self):
+        for i in self.rule:
+            break
     def checkRule():
         pass
 
