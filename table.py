ac882a3065c8c85e82c8eac09f252a48851820e8
mboyd020 <mboyd020@ucr.edu>
2023-12-12 15:48:46 -0800

    new attempt at making truth from rules

--- table.py
+++ table.py
@@ -1,36 +1,28 @@
-import rule
-import math
 class Table:
-    def __init__(self, numVars, rule):
-        self.truthTable = []
-        self.numVars = numVars
-        self.rule = rule
-    def binarizeNum(self, num, expSize):
-        returnString = ""
-        while(num > 0):
-            returnString += str(num % 2)
-            num = int(num / 2)
-        while(len(returnString) < expSize):
-            returnString = "0" + returnString
-        print(returnString)
-        return returnString
-    def evalStatement(self, vars):
-        if len(self.rule.ruleStack) == 1:
-            if self.rule.ruleStack[0].ruleStack[1] == 'v':
-                if "1" in vars:
-                    return True
+    def __init__(self, vars, filename):
+        self.vars = vars
+        self.filename = filename
+    def singleLineEval(self, lineNum):
+        line = self.readLine(lineNum)
+        total = 0
+        currExpression = 0 #0 for or, 1 for and
+        for char in line:
+            if(ord(char) < 65):
+                return -1
+            elif(ord(char) < 90):
+                if(currExpression == 0):
+                    total = total + vars[ord(char) - 65]
                 else:
-                    return False
-            else:
-                if "0" in vars:
-                    return False
-                else:
-                    return True
-        return False
-    def evalTable(self):
-        for i in range(int(math.pow(2, self.numVars))):
-            binaryVal = self.binarizeNum(i, self.numVars)
-            if self.evalStatement(binaryVal):
-                self.truthTable.append(True)
-            else:
-                self.truthTable.append(False)
+                    total = total * vars[ord(char) - 65]
+            elif(ord(char) == 94):
+                currExpression = 1
+            elif(ord(char) == 118):
+                currExpression = 0
+    def readLine(self, lineNum):
+        with open(self.filename, 'r') as rule:
+            count = 0
+            for line in rule:
+                if(count == lineNum):
+                    return line.strip().replace(" ", "")
+                count += 1
+            
