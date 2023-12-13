3c74a5b3a5c9f98863d96f039ca42ccd262acf46
mboyd020 <mboyd020@ucr.edu>
2023-12-12 16:04:03 -0800

    new rule function works so far

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import rule
 import table
-#python hates recursion. most complex rule with current system is ((a^b) ^ (c^d))
-a = rule.Rule("a ^ b")
-a.partialParseRule()
-a.printRule()
-truth = table.Table(2, a)
-truth.evalTable()
-print(truth.truthTable)
+
+vars = []
+for i in range(0, 26):
+    vars.append(-1)
+vars[0] = 0
+vars[1] = 1
+vars[2] = 0
+truth = table.Table(vars, "rule.txt")
+print(truth.singleLineEval(0))
